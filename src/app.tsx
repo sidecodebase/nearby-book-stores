34139ebfe9475865c90b63202234596d7543a1be
ovpv <viktor.prasad@gmail.com>
2022-06-17 23:04:05 +0100

    base setup

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <>This is an App</>;
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+};
